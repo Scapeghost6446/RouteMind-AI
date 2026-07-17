10c5c50bf577083cdbc7e88ac3fe30594f5bcb7b
Scapeghost6446 <scapeghost82@gmail.com>
2026-07-17 07:54:18 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1515,9 +1515,5 @@ def logout():
 
 
 if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=False
-    )
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
